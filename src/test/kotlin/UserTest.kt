131fe65266562551c126d883a63792e983f070b1
Dmitry <tegorkrsk@gmail.com>
2023-12-16 03:22:12 +0300

    Add User unit test

--- src/test/kotlin/UserTest.kt
+++ src/test/kotlin/UserTest.kt
@@ -1,9 +1,23 @@
+import data.User
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class UserTest {
 
     @Test
     fun `create user and check properties`() {
-
+        val user = User(
+            100,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+        assertEquals(100, user.id)
     }
 }
