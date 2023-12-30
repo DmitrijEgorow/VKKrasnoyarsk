a3ed4c53c4b1d78722206bf1ef4a34bb9d736a9d
Dmitry <tegorkrsk@gmail.com>
2023-12-30 11:55:23 +0300

    Fixed dto class

--- src/main/kotlin/data/ResponseDTO.kt
+++ src/main/kotlin/data/ResponseDTO.kt
@@ -1,18 +1,19 @@
 package data
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class User(
     val id: Int,
-    val first_name: String?,
-    val last_name: String?,
+    @SerialName("first_name") val firstName: String?,
+    @SerialName("last_name") val lastName: String?,
     val nickname: String?,
     val city: CityDto?,
     val country: CityDto?,
-    val home_town: String?,
+    @SerialName("home_town") val homeTown: String?,
     val sex: Int?,
-    val bdate: String?,
+    @SerialName("bdate") val birthdayDate: String?,
     val deactivated: String?
 )
 
